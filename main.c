826289262f91d6a18ee37a3c60361eccca914e50
Augusto <magnoaugustofr@gmail.com>
2016-03-08 22:26:52 -0300

    teste2

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int var1, var2;
+
+
+scanf("%d", &var1);
+
+scanf("%d", &var2);
+
+printf("> %d + %d = %d\n", var1,var2,var1+var2);
+
+
+exit(0);
+
+
+	}
